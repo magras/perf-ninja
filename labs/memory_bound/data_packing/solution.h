1117171df14efacb6745759d8e3d80258570e496
magras <magras@users.noreply.github.com>
2023-01-01 13:30:07 +0300

    [memory_bound/data_packing]

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,11 +8,19 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
+#ifdef SOLUTION
+  int i : 8;
+  short s : 8;
+  long long l : 15;
+  bool b : 1;
+  float d;
+#else
   int i;
   long long l;
   short s;
   double d;
   bool b;
+#endif
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
